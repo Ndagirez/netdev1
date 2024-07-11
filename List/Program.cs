fd8e2b7789e35a2a6d77e21cfb5d4783de88e2e9
Ndagirez <ndagire210mariam@gmail.com>
2024-07-11 16:01:42 +0300

    second commit

---
+++ List/Program.cs
@@ -0,0 +1,21 @@
+﻿ class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var fibonacciNumbers = new List<int>{ 1, 1 };
+            while (true)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+
+                foreach (var item in fibonacciNumbers)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
