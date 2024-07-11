135675a2504cb778aeb38c88d8df630f983280fe
Ndagirez <ndagire210mariam@gmail.com>
2024-07-11 19:10:20 +0300

    Basic inputoutput,arguments etc

---
+++ Chapter3_ALLprojects/BasicConsoleIO/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*using System.Net;
+
+Console.WriteLine("******* Basic input output******");
+GetUserData();
+Console.ReadLine();
+static void GetUserData()
+{
+    Console.WriteLine("plese enter you name: ");
+    string userName = Console.ReadLine();
+    Console.WriteLine("Please enter your age: ");
+    string userAge = Console.ReadLine();
+    //for coloring
+    ConsoleColor prevColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    //echo to the console
+    Console.WriteLine("hello{0} You are {1} years old.", userName, userAge);
+    //restore previous color
+    Console.ForegroundColor = prevColor;
+
+
+}*/
+// John says...
+Console.WriteLine("{0}, Number {0}, Number {0}", 9);
+Console.WriteLine("{1},{2},{1}", 30, 20, 10);
+
---
+++ Chapter3_ALLprojects/Chapter3_ALLprojects/Program.cs
@@ -0,0 +1,8 @@
+﻿// Note we are explicitly returning an int, rather than void.
+// Display a message and wait for Enter key to be pressed.
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+Console.ReadLine();
+// Return an arbitrary error code.
+return -1;
