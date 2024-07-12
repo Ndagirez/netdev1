8d7687f327b5a396d35c7493c2946b58a8945714
Ndagirez <ndagire210mariam@gmail.com>
2024-07-12 12:23:15 +0300

    basic input output formating numerical data

--- Chapter3_ALLprojects/BasicConsoleIO/Program.cs
+++ Chapter3_ALLprojects/BasicConsoleIO/Program.cs
@@ -1,26 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*using System.Net;
 
-Console.WriteLine("******* Basic input output******");
-GetUserData();
-Console.ReadLine();
-static void GetUserData()
 {
-    Console.WriteLine("plese enter you name: ");
-    string userName = Console.ReadLine();
-    Console.WriteLine("Please enter your age: ");
-    string userAge = Console.ReadLine();
-    //for coloring
-    ConsoleColor prevColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    //echo to the console
-    Console.WriteLine("hello{0} You are {1} years old.", userName, userAge);
-    //restore previous color
-    Console.ForegroundColor = prevColor;
-
-
-}*/
-// John says...
-Console.WriteLine("{0}, Number {0}, Number {0}", 9);
-Console.WriteLine("{1},{2},{1}", 30, 20, 10);
-
+    Console.WriteLine("the value in various formats ");
+    Console.WriteLine("c format:{0:c}", 9999);
+    Console.WriteLine("d9 format:{0:d9}", 9999);
+    Console.WriteLine("f3 format:{0:f3}", 9999);
+    Console.WriteLine("n format:{0:n}", 9999);
+    //notice how the change in upper or lower case
+    Console.WriteLine("E format:{0:E}", 9999);
+    Console.WriteLine("e format:{0:e}", 9999);
+    Console.WriteLine("X format:{0:X}", 9999);
+    Console.WriteLine("x format:{0:x}", 9999);
+ 
+}
